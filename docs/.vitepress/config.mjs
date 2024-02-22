344b7523cb7415487bab7098d51acc0f1de5c759
Yibo Zhang <yb_zhang@mail.ustc.edu.cn>
2024-02-22 22:33:05 +0800

    init1

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/ybsite/',
   title: "Yibo Zhang's site",
   description: "A Simple Site",
   themeConfig: {
